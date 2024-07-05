b78665372fe6ae1a69fd06a90a93bdbb7860e09b
jieuuuuuun <qkdwldms12@naver.com>
2024-07-05 20:19:30 +0900

    refactor: CustomerAccessDeniedHandler, CustomerAuthenticationEntryPoint update

--- src/main/java/com/github/drug_store_be/config/security/CustomerAuthenticationEntryPoint.java
+++ src/main/java/com/github/drug_store_be/config/security/CustomerAuthenticationEntryPoint.java
@@ -13,6 +13,6 @@ import java.io.IOException;
 public class CustomerAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendRedirect("/exceptions/entrypoint");
+        response.sendRedirect("/auth/login");
     }
 }
