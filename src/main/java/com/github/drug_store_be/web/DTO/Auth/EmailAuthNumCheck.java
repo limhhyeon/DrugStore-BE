ae7191c4577cd5c376b31890d45bf3f97c9fc669
limhhyeon <honghyeon1007@gmail.com>
2024-07-07 16:06:38 +0900

    feat: Email Controller/Dto Swagger description add

--- src/main/java/com/github/drug_store_be/web/DTO/Auth/EmailAuthNumCheck.java
+++ src/main/java/com/github/drug_store_be/web/DTO/Auth/EmailAuthNumCheck.java
@@ -2,6 +2,7 @@ package com.github.drug_store_be.web.DTO.Auth;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class EmailAuthNumCheck {
+    @Schema(name = "email",description = "인증번호 받은 이메일",example = "abc@naver.com")
     private String email;
+    @Schema(name = "auth_num",description = "이메일에서 받은 인증번호",example = "116789")
     private Integer authNum;
 }
--- src/main/java/com/github/drug_store_be/web/controller/EmailController.java
+++ src/main/java/com/github/drug_store_be/web/controller/EmailController.java
@@ -4,6 +4,7 @@ import com.github.drug_store_be.service.auth.EmailService;
 import com.github.drug_store_be.web.DTO.Auth.EmailAuthNumCheck;
 import com.github.drug_store_be.web.DTO.Auth.EmailCheck;
 import com.github.drug_store_be.web.DTO.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EmailController {
     private final EmailService emailService;
+    @Operation(summary = "인증번호 요청", description = "요청한 이메일에 인증번호 보내주기")
     @PostMapping("/send")
     public ResponseDto emailSend(@RequestBody EmailCheck emailCheck){
         return emailService.emailSendResult(emailCheck.getEmail());
     }
+    @Operation(summary = "인증번호 확인", description = "받은 인증번호 동일한지 확인")
     @PostMapping("/auth-num")
     public ResponseDto authNumCheck(@RequestBody EmailAuthNumCheck emailAuthNumCheck){
         return emailService.authNumCheckResult(emailAuthNumCheck);
