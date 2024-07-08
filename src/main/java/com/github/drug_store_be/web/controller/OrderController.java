0921d2bc89b23b8a72f604190fefd79a1d9c7e54
soheeparklee <soheeparklee@gmail.com>
2024-07-09 00:28:52 +0200

    swagger: controller operation

--- src/main/java/com/github/drug_store_be/web/controller/OrderController.java
+++ src/main/java/com/github/drug_store_be/web/controller/OrderController.java
@@ -6,6 +6,7 @@ import com.github.drug_store_be.web.DTO.ResponseDto;
 import com.github.drug_store_be.web.DTO.order.ProductRegisterDto;
 import com.github.drug_store_be.web.DTO.pay.PayRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -13,15 +14,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/order")
+@Tag(name = "Order API", description = "API for payment")
 public class OrderController {
     private final OrderService orderService;
-    @Operation(summary= "장바구니에서 주문할 상품들 가져오기")
+
+    @Operation(summary= "장바구니에서 주문할 상품들 가져오기", description = "토큰으로 장바구니에서 주문할 상품들 가져오기")
     @PostMapping("/cart-to-order")
     public ResponseDto cartToOrder(@AuthenticationPrincipal CustomUserDetails customUserDetails){
         return orderService.cartToOrder(customUserDetails);
     }
 
-    @Operation(summary= "주문에서 결제로 넘어가기")
+    @Operation(summary= "주문에서 결제로 넘어가기", description = "장바구니에 추가된 상품을 가지고 결제로 넘어가")
     @PutMapping("/order-to-pay")
     public ResponseDto orderToPay(@AuthenticationPrincipal CustomUserDetails customUserDetails,
                                   @RequestBody PayRequestDto payRequestDto){
