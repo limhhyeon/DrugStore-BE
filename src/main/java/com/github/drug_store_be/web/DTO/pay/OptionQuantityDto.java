31bb6b01448b50d8f3466927ba43f20ec17b3bcc
soheeparklee <soheeparklee@gmail.com>
2024-07-09 00:28:44 +0200

    swagger: DTO schema

--- src/main/java/com/github/drug_store_be/web/DTO/pay/OptionQuantityDto.java
+++ src/main/java/com/github/drug_store_be/web/DTO/pay/OptionQuantityDto.java
@@ -2,6 +2,7 @@ package com.github.drug_store_be.web.DTO.pay;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class OptionQuantityDto {
+        @Schema(name = "option_id",description = "옵션 아이디",example = "20")
         private Integer optionId;
+        @Schema(name = "quantity",description = "재고",example = "3")
         private Integer quantity;
 }
