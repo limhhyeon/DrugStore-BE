858632f51f3ec2afbe5c5d2997c415b8580dfd74
jieuuuuuun <qkdwldms12@naver.com>
2024-07-08 21:59:05 +0900

    refactor: like controller swagger update

--- src/main/java/com/github/drug_store_be/web/controller/LikeController.java
+++ src/main/java/com/github/drug_store_be/web/controller/LikeController.java
@@ -5,6 +5,7 @@ import com.github.drug_store_be.service.like.LikeService;
 import com.github.drug_store_be.web.DTO.Like.LikeRequest;
 import com.github.drug_store_be.web.DTO.Like.MyLikesResponse;
 import com.github.drug_store_be.web.DTO.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -19,18 +20,21 @@ public class LikeController {
 
     private final LikeService likeService;
 
+    @Operation(summary = "내가 좋아요한 목록 조회", description = "내가 좋아요 추가한 목록 조회합니다.")
     @GetMapping
     public ResponseDto getLike(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         List<MyLikesResponse> myLikesResponseList = likeService.getMyLikes(customUserDetails);
         return new ResponseDto(HttpStatus.OK.value(),"조회 성공", myLikesResponseList );
     }
 
+    @Operation(summary = "좋아요 추가", description = "관심있는 상품 좋아요 추가합니다.")
     @PostMapping
     public ResponseDto addLike(@AuthenticationPrincipal CustomUserDetails customUserDetails,
                                @RequestBody LikeRequest likeRequest) {
         return likeService.addMyLike(customUserDetails, likeRequest.getProductId());
     }
 
+    @Operation(summary = "좋아요 삭제", description = "관심있었던 상품 좋아요 삭제합니다.")
     @DeleteMapping
     public ResponseDto deleteLike(@AuthenticationPrincipal CustomUserDetails customUserDetails,
                                   @RequestBody LikeRequest likeRequest) {
