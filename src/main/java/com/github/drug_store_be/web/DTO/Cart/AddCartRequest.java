79c950eb3cae2087af35d5e3f4a2c833be1a1062
soheeparklee <soheeparklee@gmail.com>
2024-07-08 23:45:07 +0200

    swagger: DTO schema

--- src/main/java/com/github/drug_store_be/web/DTO/Cart/AddCartRequest.java
+++ src/main/java/com/github/drug_store_be/web/DTO/Cart/AddCartRequest.java
@@ -2,6 +2,7 @@ package com.github.drug_store_be.web.DTO.Cart;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,10 @@ import java.util.List;
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class AddCartRequest {
+    @Schema(name = "product_id",description = "상품 아이디",example = "1")
     private Integer productId;
+    @Schema(name = "quantity",description = "장바구니에 추가할 개수",example = "5")
     private Integer quantity;
+    @Schema(name = "options_id",description = "옵션 아이디",example = "7")
     private Integer optionsId;
 }
