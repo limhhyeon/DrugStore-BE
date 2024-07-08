78f131916d0b63675d44d0092720f0d8a4002033
soheeparklee <soheeparklee@gmail.com>
2024-07-08 23:10:38 +0200

    swagger: product register DTO schema

--- src/main/java/com/github/drug_store_be/web/DTO/order/ProductPhotoRegisterDto.java
+++ src/main/java/com/github/drug_store_be/web/DTO/order/ProductPhotoRegisterDto.java
@@ -2,6 +2,7 @@ package com.github.drug_store_be.web.DTO.order;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ProductPhotoRegisterDto {
+    @Schema(name = "photoUrl",description = "상품 이미지 저장 URL",example = "s3.png")
     private String photoUrl;
+    @Schema(name = "photoType",description = "메인 이미지 여부",example = "true")
     private boolean photoType;
 }
