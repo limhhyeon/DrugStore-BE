cb787dff5527060816c83471961d12b326191c76
soheeparklee <soheeparklee@gmail.com>
2024-07-09 15:27:07 +0200

    swagger: config https fix

--- src/main/java/com/github/drug_store_be/config/SwaggerConfig.java
+++ src/main/java/com/github/drug_store_be/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.github.drug_store_be.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
@@ -8,7 +9,17 @@ import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@OpenAPIDefinition
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "DrugstoreShop project",
+                description = "원하는 상품을 장바구니에 담아 살 수 있는 기능을 제공합니다",
+                version = "1.0.0"
+        ),
+        servers = {
+                @Server(url = "https://drugstoreproject.shop", description = "Generated server url")
+        }
+)
+
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI(){
