5691e0a5b5c496da9834a7f1fbfb86deae5f558b
jieuuuuuun <qkdwldms12@naver.com>
2024-07-10 22:17:44 +0900

    refactor:SwaggerConfig update

--- src/main/java/com/github/drug_store_be/config/SwaggerConfig.java
+++ src/main/java/com/github/drug_store_be/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.github.drug_store_be.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,13 +25,23 @@ import org.springframework.context.annotation.Configuration;
 
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI(){
-        return new OpenAPI()
-                .info(new Info()
-                        .title("DrugstoreShop project")
-                        .description("원하는 상품을 장바구니에 담아 살 수 있는 기능을 제공합니다")
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("DrugstoreShop project")
+                        .description("원하는 상품을 장바구니에 담아 살 수 있는 기능을 제공합니다.")
                         .version("1.0.0"));
+    }
 
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization")
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 
 }
